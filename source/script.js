baddbbba37f0646d99046345024d1bd831408466
Max S. Haberman <max@surveycto.com>
2024-03-13 20:56:11 -0400

    Created basic decryption field plug-in using SubtleCrypto.

--- source/script.js
+++ source/script.js
@@ -1,24 +1,21 @@
-/* global getMetaData, setMetaData, setAnswer, goToNextField, fieldProperties, getPluginParameter */
+// /* global setAnswer, fieldProperties */
 
 // var fieldProperties = {
 //   CURRENT_ANSWER: '',
 //   READONLY: false,
 //   PARAMETERS: [
 //     {
-//       "key": "passkey",
-//       "value": 'My passkey'
+//       "key": "key",
+//       // "value": 'J0UhmAygFQY6OnVAEug5tg=='
+//       "value": 'J0UhmAygFQY6OnVAEug5tg='
 //     },
 //     {
 //       "key": "iv",
-//       "value": "+8ABSnmUTxyPMECzId0shQ=="
-//     },
-//     {
-//       "key": "salt",
-//       "value": "ZJSalWToHy0="
+//       "value": "gIiYTz5exF2q4Pw0bL5oKg=="
 //     },
 //     {
 //       "key": "ciphertext",
-//       "value": "tw8+DLFuhi0YhrBHjoGN"
+//       "value": "S+iTPOq1mSx4PQrcLyTcqA=="
 //     }
 //   ]
 // }
@@ -36,51 +33,54 @@ var numParameters = parameters.length
 
 var passkey
 var iv
-var salt = ''
 var separator = '|'
 for (var p = 0; p < numParameters; p++) {
   var pa = parameters[p]
   var key = pa['key']
   var value = pa['value']
-  if (key == 'passkey') {
-    passkey = value
-  } else if (key == 'iv') {
-    iv = value
-  } else if (key == 'salt') {
-    salt = value
-  } else if (key == 'separator') {
-    separator = value
-  } else {
-    cipherData.push(value)
-  }
-}
-
-var plaintext = []
-var displayHtml = []
-for (var c = 0; c < cipherData.length; c++) {
-  var decrypted = decrypt(cipherData[c], passkey, iv, salt)
-  plaintext.push(decrypted)
-  displayHtml.push(`<p>${decrypted}</p>`)
-}
-
-setAnswer(plaintext.join(separator))
-
-document.querySelector('#decrypted').innerHTML = displayHtml.join('\n')
-
-function decrypt (cipherText, passkey, iv, salt = '', mode = 'ccm') {
-  var dData = {
-    'iv': iv,
-    'ct': cipherText,
-    'salt': salt,
-    'mode': mode
-  }
-  try {
-    return sjcl.decrypt(passkey, JSON.stringify(dData))
-  } catch (e) {
-    if (e['message'].indexOf('tag doesn\'t match') > -1) {
-      return '(Unable to decrypt. Please check your passkey and other encryption details.)'
-    } else {
-      throw e
+  switch (key) {
+    case 'key': {
+      passkey = value
+      break
+    }
+    case 'iv': {
+      iv = value
+      break
+    }
+    case 'separator': {
+      separator = value
+      break
+    }
+    default: {
+      cipherData.push(value)
     }
   }
+}
+
+decryptAll()
+
+async function decryptAll () {
+  var plaintext = []
+  var displayHtml = []
+  for (var c = 0; c < cipherData.length; c++) {
+    let decrypted = await decrypt(cipherData[c], passkey, iv)
+    plaintext.push(decrypted)
+    displayHtml.push(`<p>${decrypted}</p>`)
+  }
+
+  setAnswer(plaintext.join(separator))
+
+  document.querySelector('#decrypted').innerHTML = displayHtml.join('\n')
+}
+
+async function decrypt (ciphertext, key, iv, mode = 'cbc') {
+  // try {
+  return await subtleDecrypt(ciphertext, key, iv)
+  // } catch (e) {
+  //   if (e['message'].indexOf('tag doesn\'t match') > -1) {
+  //     return '(Unable to decrypt. Please check your passkey and other encryption details.)'
+  //   } else {
+  //     throw e
+  //   }
+  // }
 }
